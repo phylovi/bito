ee2ef4248060bc2c5ae63f52779e6095712cd44d
Erick Matsen <matsen@fredhutch.org>
2019-06-06 15:49:50 -0700

    External C library ok

--- test/test.cpp
+++ test/test.cpp
@@ -1,8 +1,7 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#define CATCH_CONFIG_MAIN
 #include "catch.hpp"
-
-unsigned int Factorial( unsigned int number ) {
-    return number <= 1 ? number : Factorial(number-1)*number;
+extern "C" {
+    #include "sbn.h"
 }
 
 TEST_CASE( "Factorials are computed", "[factorial]" ) {
