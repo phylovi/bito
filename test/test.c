459cbab605bac6ab6951e07e0f879ee4ad23f5c7
Erick Matsen <matsen@fredhutch.org>
2019-06-17 01:37:46 -0700

    test pass message

--- test/test.c
+++ test/test.c
@@ -5,7 +5,7 @@
 int main(int argc, char** argv) {
   struct MyClass* c = newMyClass();
   MyClass_int_set(c, 3);
-  printf("%i\n", MyClass_int_get(c));
   munit_assert_int(MyClass_int_get(c), ==, 3);
   deleteMyClass(c);
+  printf("Tests pass\n");
 }
