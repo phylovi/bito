734974c9bd0df7a8936bb03336323b75c9ddd86d
Erick Matsen <matsen@fredhutch.org>
2019-06-19 13:48:01 -0700

    deconfabulating

--- bison-example/newick/driver.cc
+++ bison-example/newick/driver.cc
@@ -9,9 +9,9 @@ int driver::parse(const std::string &f) {
   file = f;
   location.initialize(&file);
   scan_begin();
-  yy::parser parse(*this);
-  parse.set_debug_level(trace_parsing);
-  int res = parse();
+  yy::parser parserObject(*this);
+  parserObject.set_debug_level(trace_parsing);
+  int res = parserObject();
   scan_end();
   return res;
 }
