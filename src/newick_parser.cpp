fb7ebc03348eea8a696db3c67996eb1846c92bcd
Erick Matsen <matsen@fredhutch.org>
2019-06-20 14:34:44 -0700

    quiet option

--- src/newick_parser.cpp
+++ src/newick_parser.cpp
@@ -3,15 +3,22 @@
 
 int main(int argc, char *argv[]) {
   Driver drv;
+  bool print_trees = true;
   for (int i = 1; i < argc; ++i)
     if (argv[i] == std::string("-p"))
       drv.trace_parsing_ = true;
     else if (argv[i] == std::string("-s"))
       drv.trace_scanning_ = true;
+    else if (argv[i] == std::string("-q"))
+      print_trees = false;
     else {
       auto trees = drv.ParseFile(argv[i]);
-      for (auto tree : *trees) {
-        std::cout << tree->ToNewick() << std::endl;
+      if (print_trees) {
+        for (auto tree : *trees) {
+          std::cout << tree->ToNewick() << std::endl;
+        }
+      } else {
+        std::cout << "Parsed " << trees->size() << " trees.\n";
       }
     }
   return 0;
