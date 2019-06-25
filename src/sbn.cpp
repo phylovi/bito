d02ea75eec6481740aa88e371580f78dd9f47704
Erick Matsen <matsen@fredhutch.org>
2019-06-25 14:49:57 -0700

    Dropping C interface libsbn.

--- src/sbn.cpp
+++ src/sbn.cpp
@@ -9,16 +9,3 @@ PYBIND11_MODULE(sbn, m) {
       .def("parse_file", &SBNInstance::ParseFile)
       .def("print_status", &SBNInstance::PrintStatus);
 }
-
-
-// int add(int i, int j) {
-//   struct SBNInstance* inst = sbn_NewInstance();
-//   sbn_PrintStatus(inst);
-//   return i + j;
-// }
-//
-// PYBIND11_MODULE(example, m) {
-//     m.doc() = "pybind11 example plugin"; // optional module docstring
-//
-//     m.def("add", &add, "A function which adds two numbers");
-// }
