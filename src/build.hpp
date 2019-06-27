662849d12407ca453b4bd25bb3248d7cf5587ba4
Erick Matsen <matsen@fredhutch.org>
2019-06-27 04:51:04 -0700

    no default constructor needed!

--- src/build.hpp
+++ src/build.hpp
@@ -1,3 +1,4 @@
+#include <cassert>
 #include <unordered_map>
 #include "bitset.hpp"
 #include "tree.hpp"
@@ -5,15 +6,17 @@
 typedef std::unordered_map<uint64_t, Bitset> TagToBitsetMap;
 // typedef std::unordered_map<Bitset, std::pair<int, int>> ParamIndexer;
 
+// Using insert and at avoids needing to make a default constructor.
+
 TagToBitsetMap MakeTagToBitsetMap(Node::NodePtr t) {
   TagToBitsetMap m;
   auto leaf_count = t->LeafCount();
   t->PreOrder([&m, leaf_count](Node* n) {
     Bitset x((size_t)leaf_count);
-    // for (auto child : n->Children()) {
-    //   Bitset::AndWith(x, m[child->Tag()]);
-    //   m[n->Tag()] = x;
-    // }
+    for (auto child : n->Children()) {
+      Bitset::AndWith(x, m.at(child->Tag()));
+      assert(m.insert(std::make_pair(n->Tag(), x)).second);
+    }
   });
   return m;
 }
