04933a6be4159d11dc61c169141cfc0656fb9833
Erick Matsen <matsen@fredhutch.org>
2019-06-29 04:53:15 -0700

    DefaultDict compiles

--- src/default_dict.hpp
+++ src/default_dict.hpp
@@ -13,30 +13,30 @@
 // I would have just had a polymorphic function to do things, but we need to
 // store a default value.
 
-template <
-    class Key, class Value,
-    class Comparator = typename std::unordered_map<Key, Value>::key_compare,
-    class Allocator = typename std::unordered_map<Key, Value>::allocator_type>
+template <class Key, class T,
+          class Hash = typename std::unordered_map<Key, T>::Hash,
+          class KeyEqual = typename std::unordered_map<Key, T>::KeyEqual,
+          class Allocator = typename std::unordered_map<Key, T>::Allocator>
 class DefaultDict
-    : private std::unordered_map<Key, Value, Comparator, Allocator> {
+    : private std::unordered_map<Key, T, Hash, KeyEqual, Allocator> {
  private:
-  const Value default_value_;
+  const T default_value_;
 
  public:
-  explicit DefaultDict(Value default_value) : default_value_(default_value){};
-  using std::unordered_map<Key, Value, Comparator, Allocator>::size;
+  explicit DefaultDict(T default_value) : default_value_(default_value){};
+  using std::unordered_map<Key, T, Hash, KeyEqual, Allocator>::size;
 
-  Value& at(const Key &key) {
+  T &at(const Key &key) {
     auto search = this->find(key);
     if (search == this->end()) {
       // TODO this seems like a problem. Since we are returning a reference we
       // could change default_value.
       return default_value_;
     }
-    return std::unordered_map<Key, Value, Comparator, Allocator>::at(key);
+    return std::unordered_map<Key, T, Hash, KeyEqual, Allocator>::at(key);
   }
 
-  void increment(const Key &key, const Value &value) {
+  void increment(const Key &key, const T &value) {
     auto search = this->find(key);
     if (search == this->end()) {
       assert(this->insert(std::make_pair(key, value)).second);
@@ -49,7 +49,7 @@ class DefaultDict
 
 #ifdef DOCTEST_LIBRARY_INCLUDED
 
-TEST_CASE("DefaultDict") { auto d = DefaultDict<int, int>(); }
+TEST_CASE("DefaultDict") { auto d = std::unordered_map<int, int>(); }
 
 #endif  // DOCTEST_LIBRARY_INCLUDED
 
