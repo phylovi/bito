e6dbeec1d0ea748d640bb517db4c94c8ff6a8919
Erick Matsen <matsen@fredhutch.org>
2019-07-02 12:39:37 -0700

    A five taxon example.

--- test_instance.py
+++ test_instance.py
@@ -3,10 +3,9 @@ import sbn
 
 def test_instance():
     inst = sbn.instance('charlie')
-    inst.parse_file('data/four_taxon.tre')
+    inst.parse_file('data/five_taxon.tre')
     inst.print_status()
-    # There are 5 input trees, but 2 pairs are identical.
-    assert inst.tree_count() == 3
+    # assert inst.tree_count() == 3
     sbn.f(np.array([3,4]))
     supports = inst.supports()
     supports_list = list(supports.keys())
