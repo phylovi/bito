508c3c1657c4dfb4d655774653091242e83cd7d0
Erick Matsen <ematsen@gmail.com>
2019-08-29 11:13:10 -0700

    wip reparam trick gradient [skip travis]

---
+++ python/branch_length.py
@@ -0,0 +1,20 @@
+def param_grad(q_distribution, phylo_gradient, x, loc, shape):
+    """The gradient with respect to the parameters of the variational
+    distribution using the reparametrization trick. See (7) of the 2018 ICLR
+    paper.
+
+    This explodes if x == loc.
+    """
+    # Recall that the branch lengths are parameterized in terms of the variational
+    # parameters (epsilon is considered fixed) so when we take their derivative we need
+    # to take the branch length derivative then do the chain rule. The product of these
+    # first two terms are that.
+    d_log_probs_ratio = phylo_gradient - q_distribution.log_prob_grad(x, loc, shape)
+    d_reparam_loc, d_reparam_shape = q_distribution.reparam_grad(x, loc, shape)
+    # This is the derivative with respect to the variational parametrization itself.
+    d_q_loc, d_q_shape = q_distribution.log_prob_param_grad(x, loc, shape)
+    d_loc = d_log_probs_ratio * d_reparam_loc - d_q_loc
+    d_shape = d_log_probs_ratio * d_reparam_shape - d_q_shape
+    return d_loc, d_shape
+
+
--- python/distributions.py
+++ python/distributions.py
@@ -2,31 +2,56 @@ import numpy as np
 
 
 class Normal(object):
+    """This class implements a multidimensional normal distribution such that
+    each dimension has a mu and a sigma.
+
+    The given dimension is laid out on axis 1, which in our application
+    is across branches of a tree.
+
+    pdf(x; mu, sigma) = exp(-0.5 (x - mu)**2 / sigma**2) / Z
+    Z = (2 pi sigma**2)**0.5
+    """
+
     def __init__(self, dim):
-        self.d = dim
+        self.dim = dim
 
     def sample(self, mu, sigma, n_particles=1):
+        """mu and sigma should be dim-dimensional."""
+        assert len(mu) == self.dim
+        assert len(sigma) == self.dim
         if n_particles > 1:
-            return np.random.normal(loc=mu,
-                                    scale=np.abs(sigma),
-                                    size=(n_particles, self.d))
+            return np.random.normal(
+                loc=mu, scale=np.abs(sigma), size=(n_particles, self.dim)
+            )
         else:
             return np.random.normal(loc=mu, scale=np.abs(sigma))
 
-    def log_prob(self, x, mu, sigma, grad=False):
-        ratio = np.exp(np.log((x - mu)**2) - np.log(sigma**2))
-        if grad:
-            return ratio / (mu - x)
-        else:
-            return np.sum(
-                -0.5 * np.log(2 * np.pi) - 0.5 * np.log(sigma**2) -
-                0.5 * ratio,
-                axis=1,
-            )
+    def log_prob(self, x, mu, sigma):
+        """Given an ensemble of dim-dimensional samples, calculate the joint
+        probability (across dim) for each one.
+
+        Thus, x is an array that is samples on axis 0 and branches on
+        axis 1.
+        """
+        ratio = self._internal_ratio(x, mu, sigma)
+        return np.sum(
+            -0.5 * np.log(2 * np.pi) - 0.5 * np.log(sigma ** 2) - 0.5 * ratio, axis=1
+        )
+
+    def log_prob_grad(self, x, mu, sigma):
+        """The gradient of log_prob."""
+        return (x - mu) / sigma ** 2
+
+    def log_prob_param_grad(self, x, mu, sigma):
+        """The derivative of a the log of distribution with respect to its
+        parameters."""
+        ratio = self._internal_ratio(x, mu, sigma)
+        return ratio / (x - mu), -1.0 / sigma * (1.0 - ratio)
 
     def reparam_grad(self, x, mu, sigma):
         """The gradient component from the reparametrization trick. In the
-        reparametrization trick for the normal distribution, our normal
+        reparametrization trick for the normal distribution, our normal.
+
         variate is mu + sigma * epsilon. Here we take the derivative of that
         with respect to mu and sigma.
         """
@@ -36,8 +61,13 @@ class Normal(object):
         std_x = (x - mu) / sigma
         return np.ones(mu.shape), std_x
 
-    def lp_param_grad(self, x, mu, sigma):
-        """The derivative of a the log of distribution with respect to its
-        parameters."""
-        ratio = np.exp(np.log((x - mu)**2) - np.log(sigma**2))
-        return ratio / (x - mu), -1.0 / sigma * (1.0 - ratio)
+    def internal_ratio(self, x, mu, sigma):
+        return self._internal_ratio(x, mu, sigma)
+
+    def _internal_ratio(self, x, mu, sigma):
+        """(x - mu)**2 / sigma**2
+
+        Cheng used
+        ratio = np.exp(np.log((x - mu) ** 2) - np.log(sigma ** 2))
+        """
+        return (x - mu) ** 2 / sigma ** 2
