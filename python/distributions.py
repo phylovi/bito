2208532713b0b51f3304f0096d1eae7d096708d9
Erick Matsen <ematsen@gmail.com>
2019-09-05 14:31:38 -0700

    Adding LogNormal

--- python/distributions.py
+++ python/distributions.py
@@ -31,22 +31,70 @@ class Gamma(object):
         return (alpha - 1) / x - beta
 
 
-class ExpGamma(object):
-    """Distribution of an X whose exp is Gamma distributed.
+class LogNormal(object):
+    """This class implements a multidimensional normal distribution such that
+    each dimension has a mu and a sigma.
 
-    TODO details if kept.
+    The given dimension is laid out on axis 1, which in our application
+    is across branches of a tree.
+
+    pdf(x; mu, sigma) = exp(-0.5 (log(x) - mu)**2 / sigma**2) / (x * Z)
+    Z = (2 pi sigma**2)**0.5
     """
 
     def __init__(self, dim):
         self.dim = dim
 
-    def log_prob(self, x, alpha, beta):
-        return np.sum(stats.gamma.logpdf(np.exp(x), alpha, scale=(1 / beta)), axis=1)
+    def sample(self, mu, sigma, n_particles=1):
+        """Sample from the normal.
 
-    def log_prob_grad(self, x, alpha, beta):
+        mu and sigma should be dim-dimensional.
+        """
+        assert len(mu) == self.dim
+        assert len(sigma) == self.dim
+        return np.random.lognormal(
+            loc=mu, scale=np.abs(sigma), size=(n_particles, self.dim)
+        )
+
+    def log_prob(self, x, mu, sigma):
+        """Given an ensemble of dim-dimensional samples, calculate the joint
+        probability (across dim) for each one.
+
+        Input x is an array that is samples on axis 0 and branches on
+        axis 1. The output is a 1-d array of dimension equal to the
+        number of samples.
+        """
+        assert x.ndim == 2
+        ratio = self._internal_ratio(x, mu, sigma)
+        return np.sum(
+            -np.log(x) - 0.5 * (ratio + np.log(2 * np.pi) + np.log(sigma ** 2)), axis=1
+        )
+
+    def log_prob_grad(self, x, mu, sigma):
         """The gradient of log_prob."""
-        exp_x = np.exp(x)
-        return ((alpha - 1) / exp_x - beta) * exp_x
+        return (-1 / x) * (1 + (np.log(x) - mu) / sigma ** 2)
+
+    def log_prob_param_grad(self, x, mu, sigma):
+        """The derivative of a the log of distribution with respect to its
+        parameters."""
+        ratio = self._internal_ratio(x, mu, sigma)
+        return ratio / (np.log(x) - mu), -1.0 / sigma * (1.0 - ratio)
+
+    def reparam_grad(self, x, mu, sigma):
+        """The gradient component from the reparametrization trick.
+
+        In the reparametrization trick for the normal distribution, our normal
+        variate is exp(mu + sigma * epsilon). Here we take the derivative of that
+        with respect to mu and sigma.
+        """
+        # We translate from x to the epsilon that would have made it.
+        epsilon = (x - mu) / sigma
+        return x, x * epsilon
+
+    def _internal_ratio(self, x, mu, sigma):
+        """(log(x) - mu)**2 / sigma**2
+        """
+        return (np.log(x) - mu) ** 2 / sigma ** 2
 
 
 class Normal(object):
--- python/test_distributions.py
+++ python/test_distributions.py
@@ -1,5 +1,6 @@
 import distributions
 
+import numpy as np
 from pytest import approx
 import tensorflow as tf
 import tensorflow_probability as tfp
@@ -13,8 +14,8 @@ beta = 0.6
 params = tf.constant([x, alpha, beta])
 with tf.GradientTape() as g:
     g.watch(params)
-    gamma = tfp.distributions.Gamma(concentration=params[1], rate=params[2])
-    y = gamma.log_prob(params[0])
+    tf_distribution = tfp.distributions.Gamma(concentration=params[1], rate=params[2])
+    y = tf_distribution.log_prob(params[0])
 gradient = g.gradient(y, params).numpy()
 g = distributions.Gamma(1)
 assert gradient[0] == approx(g.log_prob_grad(x, alpha, beta))
@@ -26,9 +27,26 @@ shape = 0.6
 params = tf.constant([x, loc, shape])
 with tf.GradientTape() as g:
     g.watch(params)
-    normal = tfp.distributions.Normal(loc=params[1], scale=params[2])
-    y = normal.log_prob(params[0])
+    tf_distribution = tfp.distributions.Normal(loc=params[1], scale=params[2])
+    y = tf_distribution.log_prob(params[0])
 gradient = g.gradient(y, params).numpy()
 d = distributions.Normal(1)
 assert gradient[0] == approx(d.log_prob_grad(x, loc, shape))
+assert (gradient[1], gradient[2]) == approx(d.log_prob_param_grad(x, loc, shape))
+
+# Test for LogNormal
+x = 0.3
+loc = 0.55
+shape = 0.6
+params = tf.constant([x, loc, shape])
+# TODO make x_arr at the top?
+x_arr = np.array([[x]])
+with tf.GradientTape() as g:
+    g.watch(params)
+    tf_distribution = tfp.distributions.LogNormal(loc=params[1], scale=params[2])
+    y = tf_distribution.log_prob(params[0])
+gradient = g.gradient(y, params).numpy()
+d = distributions.LogNormal(1)
+assert y.numpy() == approx(d.log_prob(x_arr, loc, shape)[0])
+assert gradient[0] == approx(d.log_prob_grad(x_arr, loc, shape))
 assert (gradient[1], gradient[2]) == approx(d.log_prob_param_grad(x, loc, shape))
