4a6d126d29b3f5e8de092d1f5bb2459437dc6ee9
Erick Matsen <ematsen@gmail.com>
2019-09-06 05:14:23 -0700

    [skip travis]

--- python/branch_length.py
+++ python/branch_length.py
@@ -25,7 +25,7 @@ def like_weights(q_distribution, phylo_log_likelihoods, x, loc, shape, clip):
     See just before (6) in the 2018 ICLR paper.
     """
     log_prob_ratio = phylo_log_likelihoods - q_distribution.log_prob(x, loc, shape)
-    #if clip:
+    # if clip:
     #    log_prob_ratio = np.clip(log_prob_ratio, -clip, clip)
     return sp.special.softmax(log_prob_ratio)
 
@@ -34,8 +34,11 @@ def param_grad(q_distribution, weights, phylo_gradient, x, loc, shape):
     """The gradient with respect to the parameters of the variational
     distribution using the reparametrization trick.
 
+    We expect x to be two dimensional, with samples on axis 0 and branches on axis 1.
+
     See (7) of the 2018 ICLR paper.
     """
+    assert phylo_gradient.ndim == 2
     # Recall that the branch lengths are parameterized in terms of the variational
     # parameters (epsilon is considered fixed) so when we take their derivative we need
     # to take the branch length derivative:
@@ -51,5 +54,5 @@ def param_grad(q_distribution, weights, phylo_gradient, x, loc, shape):
     d_shape = d_log_prob_ratio * d_reparam_shape - d_q_shape
     # Our multiple samples are laid out on axis 0, so this multiplication on the left
     # reweights them.
-    weights = np.ones(weights.shape)/len(weights)
+    weights = np.ones(weights.shape) / len(weights)
     return np.matmul(weights, d_loc), np.matmul(weights, d_shape)
