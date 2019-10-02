b86ebda49cab2fc316ef10a86b660bc7e4039b7c
Erick Matsen <ematsen@gmail.com>
2019-10-02 11:35:33 -0700

    [skip travis]

--- setup.py
+++ setup.py
@@ -3,15 +3,16 @@
 
 import sys, subprocess, os.path
 
-sys.path[0:0] = ['setup-requires']
+sys.path[0:0] = ["setup-requires"]
 
 try:
     import enscons.setup
 except ImportError:
     requires = ["enscons"]
-    subprocess.check_call([sys.executable, "-m", "pip", "install", 
-        "-t", "setup-requires"] + requires)
-    del sys.path_importer_cache['setup-requires'] # needed if setup-requires was absent
+    subprocess.check_call(
+        [sys.executable, "-m", "pip", "install", "-t", "setup-requires"] + requires
+    )
+    del sys.path_importer_cache["setup-requires"]  # needed if setup-requires was absent
     import enscons.setup
 
 enscons.setup.setup()
