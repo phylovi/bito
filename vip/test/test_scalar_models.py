0984360bb234b76a53df590abbc6100379ffc50c
Erick Matsen <ematsen@gmail.com>
2019-10-21 11:40:09 -0700

    [skip travis]

--- vip/test/test_scalar_models.py
+++ vip/test/test_scalar_models.py
@@ -9,14 +9,14 @@ tfd = tfp.distributions
 
 
 def test_lognormal_log_prob():
+    sample = np.array([0.3, 3.14159, 0.0001])
     loc = 0.4
     scale = 1.3
-    log_normal = models.LogNormalModel(np.array([loc, scale]), 1)
-    tf_log_normal = models.of_name("tf_lognormal", variable_count=1)
+    log_normal = models.LogNormalModel(np.array([loc, scale]), len(sample))
+    tf_log_normal = models.of_name("tf_lognormal", variable_count=len(sample))
     tf_log_normal.q_params[:, 0] = loc
     tf_log_normal.q_params[:, 1] = scale
-    sample = np.array([0.3])
-    which_variables = np.array([0])
+    which_variables = np.arange(len(sample))
     ours = log_normal.log_prob(sample, which_variables=which_variables)
     theirs = tf_log_normal.log_prob(sample, which_variables=which_variables)
     assert ours == approx(theirs)
