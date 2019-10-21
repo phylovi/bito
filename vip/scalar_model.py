f93d7ef5e6a82a52f3b918a4583b3926f2284ed0
Erick Matsen <ematsen@gmail.com>
2019-10-21 11:20:03 -0700

    tf models run again
    [skip travis]

--- vip/scalar_model.py
+++ vip/scalar_model.py
@@ -52,12 +52,15 @@ class ScalarModel(abc.ABC):
 
         On output:
 
-        * sample is the desired number of samples.
+        * sample is the desired sample.
         * dg_dpsi is the gradient of the reparametrization function with respect to the
           scalar model parameters, and is laid out as (particle, variable, parameter).
-        * dlog_sum_q_dpsi is the gradient of the sum of the q function over particles
+        * dlog_qg_dpsi is the gradient of the sum of the q function over particles
           with respect to the scalar model parameters, and is laid out as (variable,
           parameter).
+
+        For both of the last two arrays, the variable indexing is in terms of the
+        complete set of variables.
 
         See the tex for details.
         """
@@ -119,11 +122,11 @@ class LogNormalModel(ScalarModel):
         return sample
 
     def sample_and_gradients(self, particle_count, px_which_variables):
-        # Comments of the form eq:XXX refer to equations in the tex.
+        # Comments of the form eq:XX refer to equations in the tex.
         # We check that px_which_variables is a fixed width in the loop below.
         which_variables_size = px_which_variables[0].size
         sample = np.empty((particle_count, which_variables_size))
-        dg_dpsi = np.empty((particle_count, self.variable_count, 2))
+        dg_dpsi = np.zeros((particle_count, self.variable_count, 2))
         dlog_qg_dpsi = np.zeros((particle_count, self.variable_count, 2))
         # eq:dlogqgdPsi
         dlog_qg_dpsi[:, :, 0] = -1.0
@@ -144,7 +147,7 @@ class LogNormalModel(ScalarModel):
         return (sample, dg_dpsi, dlog_qg_dpsi)
 
     def log_prob(self, values, which_variables):
-        """Return a log probability for the given values.
+        """Return the log joint probability of the given values.
 
         The density is:
         frac{1}{x sigma sqrt{2 pi}} exp(-frac{(log x - mu)^2}{2 sigma^2})
@@ -202,8 +205,8 @@ class TFScalarModel(ScalarModel):
     See ScalarModel for more information.
     """
 
-    def __init__(self, q_factory, initial_params, variable_count, particle_count):
-        super().__init__(initial_params, variable_count, particle_count)
+    def __init__(self, q_factory, initial_params, variable_count):
+        super().__init__(initial_params, variable_count)
         self.q_factory = q_factory
         self.name = "TF" + self.q_factory(np.array([initial_params])).name
 
@@ -227,30 +230,50 @@ class TFScalarModel(ScalarModel):
         else:
             print("Mode matching not implemented for " + self.name)
 
-    def sample(self, particle_count, log_prob=False):
-        q_distribution = self.q_factory(self.q_params)
-        sample = q_distribution.sample(particle_count).numpy()
-        if log_prob:
-            return sample, np.sum(q_distribution.log_prob(sample), axis=1)
-        else:
-            return sample
+    def sample(self, particle_count, px_which_variables):
+        if px_which_variables is None:
+            q_distribution = self.q_factory(self.q_params)
+            return q_distribution.sample(particle_count).numpy()
+        # else:
+        # We check that px_which_variables is a fixed width in the loop below.
+        which_variables_size = px_which_variables[0].size
+        sample = np.empty((particle_count, which_variables_size))
+        for particle_idx, which_variables in enumerate(px_which_variables):
+            assert which_variables_size == which_variables.size
+            q_distribution = self.q_factory(self.q_params[which_variables, :])
+            sample[particle_idx, :] = q_distribution.sample().numpy()
+        return sample
 
-    def sample_and_prep_gradients(self, particle_count, which_variables):
-        """Take a sample from q and prepare a gradient of the sample and of log
-        q with respect to the parameters of q."""
-        raise NotImplementedError("We don't use which_variables yet.")
-        with tf.GradientTape(persistent=True) as g:
-            tf_params = tf.constant(self.q_params, dtype=tf.float32)
-            g.watch(tf_params)
-            q_distribution = self.q_factory(tf_params)
-            tf_z = q_distribution.sample(particle_count)
-            q_term = tf.math.reduce_sum(q_distribution.log_prob(tf_z))
-        self.sample = tf_z.numpy()
-        # The Jacobian is laid out as particles x edges x edges x params.
-        self.dg_dpsi = np.sum(g.jacobian(tf_z, tf_params).numpy(), axis=2)
-        self.dlog_sum_q_dpsi = g.gradient(q_term, tf_params).numpy()
-        del g  # Should happen anyway but being explicit to remember.
-        return self.sample
+    def sample_and_gradients(self, particle_count, px_which_variables):
+        which_variables_size = px_which_variables[0].size
+        sample = np.empty((particle_count, which_variables_size))
+        dg_dpsi = np.zeros((particle_count, self.variable_count, 2))
+        dlog_qg_dpsi = np.zeros((particle_count, self.variable_count, 2))
+        for particle_idx, which_variables in enumerate(px_which_variables):
+            with tf.GradientTape(persistent=True) as g:
+                tf_params = tf.constant(
+                    self.q_params[which_variables, :], dtype=tf.float32
+                )
+                g.watch(tf_params)
+                q_distribution = self.q_factory(tf_params)
+                tf_sample = q_distribution.sample()
+                q_term = q_distribution.log_prob(tf_sample)
+            sample[particle_idx, :] = tf_sample.numpy()
+            dg_dpsi[particle_idx, which_variables, :] = g.gradient(
+                tf_sample, tf_params
+            ).numpy()
+            dlog_qg_dpsi[particle_idx, which_variables, :] = g.gradient(
+                q_term, tf_params
+            ).numpy()
+            del g  # Should happen anyway but being explicit to remember.
+        return (sample, dg_dpsi, dlog_qg_dpsi)
+
+    def log_prob(self, values, which_variables):
+        """Return the log joint probability of the given values."""
+        assert values.size == which_variables.size
+        return np.sum(
+            self.q_factory(self.q_params[which_variables, :]).log_prob(values).numpy()
+        )
 
 
 def of_name(name, *, variable_count):
@@ -259,11 +282,11 @@ def of_name(name, *, variable_count):
 
     if name == "lognormal":
         return LogNormalModel(np.array([-2.0, 0.5]), variable_count)
-    # if name == "tf_lognormal":
-    #     return build_tf_model(lognormal_factory, [-2.0, 0.5])
-    # if name == "tf_truncated_lognormal":
-    #     return build_tf_model(truncated_lognormal_factory, [-1.0, 0.5, 0.1])
-    # if name == "tf_gamma":
-    #     return build_tf_model(gamma_factory, [1.3, 3.0])
+    if name == "tf_lognormal":
+        return build_tf_model(lognormal_factory, [-2.0, 0.5])
+    if name == "tf_truncated_lognormal":
+        return build_tf_model(truncated_lognormal_factory, [-1.0, 0.5, 0.1])
+    if name == "tf_gamma":
+        return build_tf_model(gamma_factory, [1.3, 3.0])
 
     raise Exception(f"Model {name} not known.")
