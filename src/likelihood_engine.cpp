ab718eed0e2a0b527624c8e74a2a1635308555df
Erick Matsen <ematsen@gmail.com>
2019-10-25 05:52:52 -0700

    chug
    [skip travis]

--- src/likelihood_engine.cpp
+++ src/likelihood_engine.cpp
@@ -2,3 +2,12 @@
 // libsbn is free software under the GPLv3; see LICENSE file for details.
 
 #include "likelihood_engine.hpp"
+
+void LikelihoodEngine::FinalizeBeagleInstances() {
+  for (const auto &beagle_instance : beagle_instances_) {
+    Assert(beagleFinalizeInstance(beagle_instance) == 0,
+           "beagleFinalizeInstance gave nonzero return value!");
+  }
+  beagle_instances_.clear();
+}
+
