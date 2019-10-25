dac72113539b6fab75f69ce573ef8c927d2e75eb
Erick Matsen <ematsen@gmail.com>
2019-10-25 04:31:15 -0700

    likelihood engine has subs model
    [skip travis]

--- src/likelihood_engine.hpp
+++ src/likelihood_engine.hpp
@@ -4,12 +4,26 @@
 #ifndef SRC_LIKELIHOOD_ENGINE_HPP_
 #define SRC_LIKELIHOOD_ENGINE_HPP_
 
+#include <memory>
+#include <utility>
 #include "substitution_model.hpp"
 
-class LikelihoodEngine {};
+class LikelihoodEngine {
+ public:
+  explicit LikelihoodEngine(SubstitutionModelPtr substitution_model)
+      : substitution_model_(std::move(substitution_model)) {}
+
+ private:
+  SubstitutionModelPtr substitution_model_;
+};
+
 
 #ifdef DOCTEST_LIBRARY_INCLUDED
-TEST_CASE("LikelihoodEngine") {}
+TEST_CASE("LikelihoodEngine") {
+  auto substitution_model = std::make_unique<GTRModel>();
+
+  auto engine = LikelihoodEngine(std::move(substitution_model));
+}
 #endif  // DOCTEST_LIBRARY_INCLUDED
 
 #endif  // SRC_LIKELIHOOD_ENGINE_HPP_
