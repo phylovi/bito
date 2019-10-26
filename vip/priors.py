6a9417851045c41bac336490f51634c3b44317af
Erick Matsen <ematsen@gmail.com>
2019-10-26 03:57:58 -0700

    implementing the prior fix
    [skip travis]

--- vip/priors.py
+++ vip/priors.py
@@ -1,17 +1,16 @@
 import numpy as np
 
 
-def log_exp_prior(x, rate=10):
+def log_exp_prior(theta_sample, rate=10):
     """The log of the Exponential(rate) probability density.
 
-    x should have particles on axis 0 and variables on axis 1.
+    theta_sample should have particles on axis 0 and variables on axis
+    1.
     """
-    # This commented version is the correct one, and will be fixed when I address
-    # issue #116.
-    # return np.log(rate) * x.shape[1] - rate * np.sum(x, axis=1)
-    return np.log(rate) - np.sum(rate * x, axis=1)
+    assert theta_sample.ndim == 2
+    return np.log(rate) * theta_sample.shape[1] - rate * np.sum(theta_sample, axis=1)
 
 
-def grad_log_exp_prior(x, rate=10):
+def grad_log_exp_prior(theta_sample, rate=10):
     """The gradient of the log Exponential(rate) probability density."""
     return -rate
