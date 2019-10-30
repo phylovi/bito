2c4d863466923dd5c29d06b7b0f9bb1cd87242a5
Erick Matsen <ematsen@gmail.com>
2019-10-30 05:31:32 -0700

    rename
    [skip travis]

--- vip/priors.py
+++ vip/priors.py
@@ -1,16 +1,18 @@
 import numpy as np
 
 
-def log_exp_prior(theta_sample, rate=10):
+def log_exp_prior(px_theta_sample, rate=10):
     """The log of the Exponential(rate) probability density.
 
-    theta_sample should have particles on axis 0 and variables on axis
+    px_theta_sample should have particles on axis 0 and variables on axis
     1.
     """
-    assert theta_sample.ndim == 2
-    return np.log(rate) * theta_sample.shape[1] - rate * np.sum(theta_sample, axis=1)
+    assert px_theta_sample.ndim == 2
+    return np.log(rate) * px_theta_sample.shape[1] - rate * np.sum(
+        px_theta_sample, axis=1
+    )
 
 
-def grad_log_exp_prior(theta_sample, rate=10):
+def grad_log_exp_prior(px_theta_sample, rate=10):
     """The gradient of the log Exponential(rate) probability density."""
     return -rate
