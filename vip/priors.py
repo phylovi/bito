Fix discrepancy in ELBO calculation; tests (#137)
Closes #116

vip/priors.py
@@ -1,17 +1,18 @@
 import numpy as np
 
 
-def log_exp_prior(x, rate=10):
+def log_exp_prior(px_theta_sample, rate=10):
     """The log of the Exponential(rate) probability density.
 
-    x should have particles on axis 0 and variables on axis 1.
+    px_theta_sample should have particles on axis 0 and variables on axis
+    1.
     """
-    # This commented version is the correct one, and will be fixed when I address
-    # issue #116.
-    # return np.log(rate) * x.shape[1] - rate * np.sum(x, axis=1)
-    return np.log(rate) - np.sum(rate * x, axis=1)
+    assert px_theta_sample.ndim == 2
+    return np.log(rate) * px_theta_sample.shape[1] - rate * np.sum(
+        px_theta_sample, axis=1
+    )
 
 
-def grad_log_exp_prior(x, rate=10):
+def grad_log_exp_prior(px_theta_sample, rate=10):
     """The gradient of the log Exponential(rate) probability density."""
     return -rate

vip/test/test_scalar_models.py
@@ -14,7 +14,7 @@ variable_count = params.shape[0]
 def test_lognormal_log_prob():
     sample = np.array([0.3, 3.14159, 0.0001])
     which_variables = np.arange(variable_count)
-    log_normal = models.LogNormalModel(np.array([0.0, 1.0]), len(sample))
+    log_normal = models.of_name("lognormal", variable_count=len(sample))
     log_normal.q_params[:, :] = params
     tf_log_normal = models.of_name("tf_lognormal", variable_count=len(sample))
     tf_log_normal.q_params[:, :] = params