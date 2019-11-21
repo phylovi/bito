6ddab16b31be400296d65e358bb671732f125095
Erick Matsen <ematsen@gmail.com>
2019-11-21 05:43:19 -0800

    [skip travis]

--- src/block_model.cpp
+++ src/block_model.cpp
@@ -32,6 +32,18 @@ EigenVectorXdRef BlockModel::ExtractSegment(EigenVectorXdRef parameterization,
   return parameterization.segment(start_idx, parameter_count);
 }
 
+EigenMatrixXdRef BlockModel::ExtractBlock(EigenMatrixXdRef parameter_matrix,
+                                          std::string key) const {
+  auto [start_idx, parameter_count] = block_specification_.Find(key);
+  if (start_idx + parameter_count > parameter_matrix.cols()) {
+    Failwith("Model parameter " + key +
+             " request too long for a parameterization of width " +
+             std::to_string(parameter_matrix.cols()) + ".");
+  }  // else
+  return parameter_matrix.block(0, start_idx, parameter_matrix.rows(),
+                                parameter_count);
+}
+
 // In the interest of having a single code path, this implementation does twice
 // as many lookups as it needs by calling ExtractSegment. My intent is not to
 // have it be called frequently-- just once when we're setting things up. So if
@@ -45,18 +57,6 @@ BlockModel::ParameterSegmentMap BlockModel::ParameterSegmentMapOf(
                ExtractSegment(parameterization, key));
   }
   return parameter_segment_map;
-}
-
-EigenMatrixXdRef BlockModel::ExtractBlock(EigenMatrixXdRef parameter_matrix,
-                                          std::string key) const {
-  auto [start_idx, parameter_count] = block_specification_.Find(key);
-  if (start_idx + parameter_count > parameter_matrix.cols()) {
-    Failwith("Model parameter " + key +
-             " request too long for a parameterization of width " +
-             std::to_string(parameter_matrix.cols()) + ".");
-  }  // else
-  return parameter_matrix.block(0, start_idx, parameter_matrix.rows(),
-                                parameter_count);
 }
 
 BlockModel::ParameterBlockMap BlockModel::ParameterBlockMapOf(
