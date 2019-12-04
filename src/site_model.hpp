08365fc08423c67759352c5c4b3f2948c762df09
Erick Matsen <ematsen@gmail.com>
2019-12-04 05:27:45 -0800

    no references to eigen stuff
    [skip travis]

--- src/site_model.hpp
+++ src/site_model.hpp
@@ -85,7 +85,7 @@ class WeibullSiteModel : public SiteModel {
 TEST_CASE("SiteModel") {
   // Test 1: First we test using the "built in" default values.
   auto weibull_model = std::make_unique<WeibullSiteModel>(4, 1.0);
-  const Eigen::VectorXd& rates = weibull_model->GetCategoryRates();
+  const Eigen::VectorXd rates = weibull_model->GetCategoryRates();
   Eigen::VectorXd rates_r(4);
   rates_r << 0.1457844, 0.5131316, 1.0708310, 2.2702530;
   CheckVectorXdEquality(rates, rates_r, 0.0001);
@@ -96,17 +96,16 @@ TEST_CASE("SiteModel") {
   param_vector << 0.1;
   weibull_model->SetParameters(param_vector);
   rates_r << 4.766392e-12, 1.391131e-06, 2.179165e-03, 3.997819e+00;
-  const Eigen::VectorXd& rates2 = weibull_model->GetCategoryRates();
+  const Eigen::VectorXd rates2 = weibull_model->GetCategoryRates();
   CheckVectorXdEquality(rates2, rates_r, 0.0001);
 
   // Test 3: Check proportions.
-  const Eigen::VectorXd& proportions = weibull_model->GetCategoryProportions();
-  for (size_t i = 0; i < proportions.size(); i++) {
-    CHECK_LT(fabs(proportions[i] - 0.25), 0.0001);
-  }
+  const Eigen::VectorXd proportions = weibull_model->GetCategoryProportions();
+  CheckVectorXdEquality(0.25, proportions, 0.0001);
 
   // Test 4: Check sum rates[i]*proportions[i]==1.
   CHECK_LT(fabs(rates.dot(proportions) - 1.), 0.0001);
+  CHECK_LT(fabs(rates2.dot(proportions) - 1.), 0.0001);
 }
 #endif  // DOCTEST_LIBRARY_INCLUDED
 
