2b8ea5af86d76accaf420fbcb365e64a8c293393
Erick Matsen <ematsen@gmail.com>
2020-01-13 06:00:27 -0800

    docs
    [skip travis]

--- src/pylibsbn.cpp
+++ src/pylibsbn.cpp
@@ -116,6 +116,9 @@ PYBIND11_MODULE(libsbn, m) {
             Make the indexer representation of each currently stored tree.
 
             See the comment for ``IndexerRepresentationOf`` in ``sbn_maps.hpp`` to learn about what that means.
+
+            Note: any rootsplit or a PCSS that is not contained in the subsplit support is given an index equal
+            to the length of ``sbn_parameters``. No warning is given.
            )raw")
       .def("make_psp_indexer_representations",
            &SBNInstance::MakePSPIndexerRepresentations, R"raw(
