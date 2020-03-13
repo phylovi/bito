2d93959b8b1aa9a29655013d52e6729d0e7e5b0f
Erick Matsen <ematsen@gmail.com>
2020-03-13 05:14:53 -0700

    reverting noodle

--- src/noodle.cpp
+++ src/noodle.cpp
@@ -1,9 +1,5 @@
 #include "libsbn.hpp"
 
-#include <iomanip>
-#include <iostream>
-#include <sstream>
-
 // This is just a place to muck around, and check out performance.
 
 auto now = std::chrono::high_resolution_clock::now;
@@ -12,63 +8,7 @@ auto now = std::chrono::high_resolution_clock::now;
 // valgrind --tool=callgrind ./_build/noodle
 // gprof2dot -f callgrind callgrind.out.16763 | dot -Tpng -o ~/output.png
 
-std::string QuoteString(const std::string &in_str) {
-  std::stringstream ss;
-  ss << std::quoted(in_str);
-  return ss.str();
-}
-
-std::string DequoteString(const std::string &in_str) {
-  if (in_str.empty()) {
-    return std::string();
-  }
-  char delimiter = in_str.at(0);
-  if (delimiter != '\'' && delimiter != '"') {
-    return std::string(in_str);
-  }
-  std::stringstream ss(in_str);
-  std::string out_str;
-  ss >> std::quoted(out_str, delimiter);
-  return out_str;
-}
-
-TagStringMap TransformStringValues(std::function<std::string(const std::string &)> f,
-                                   const TagStringMap &in_map) {
-  TagStringMap out_map;
-  for (const auto &[tag, value] : in_map) {
-    out_map.insert({tag, f(value)});
-  }
-  return out_map;
-}
-
-TagStringMap DequoteTagStringMap(const TagStringMap &tag_string_map) {
-  return TransformStringValues(DequoteString, tag_string_map);
-}
-
 int main() {
-  std::string unquoted_test(R"raw(hello 'there" friend)raw");
-  std::string double_quoted_test(R"raw("this is a \" test")raw");
-  std::string double_quoted_dequoted(R"raw(this is a " test)raw");
-  std::string single_quoted_test(R"raw('this is a \' test')raw");
-  std::string single_quoted_dequoted(R"raw(this is a ' test)raw");
-
-  assert(QuoteString(unquoted_test) == R"raw("hello 'there\" friend")raw");
-  assert(DequoteString(double_quoted_test) == double_quoted_dequoted);
-  assert(DequoteString(single_quoted_test) == single_quoted_dequoted);
-  assert(DequoteString(QuoteString(unquoted_test)) == unquoted_test);
-
-  TagStringMap test_map(
-      {{2, unquoted_test}, {3, double_quoted_test}, {5, single_quoted_test}});
-  TagStringMap expected_test_map(
-      {{2, unquoted_test}, {3, double_quoted_dequoted}, {5, single_quoted_dequoted}});
-
-  assert(expected_test_map == DequoteTagStringMap(test_map));
-
-  // R"raw("hi 'there\" ")raw"
-  // R"raw(this is a " test)raw"
-
-  return 0;
-
   uint32_t leaf_count = 10000;
 
   Node::NodePtr topology = Node::Ladder(leaf_count);
