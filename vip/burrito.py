19c24000f40f6bf1f5f1c703a74ceb6b64ba903d
junseonghwan <s2jun.uw@gmail.com>
2020-03-16 17:12:14 -0700

    Provide an option to use vimco.

--- vip/burrito.py
+++ vip/burrito.py
@@ -34,8 +34,10 @@ class Burrito:
         optimizer_name,
         particle_count,
         thread_count=1,
+        use_vimco=True
     ):
         self.particle_count = particle_count
+        self.use_vimco = use_vimco
         self.inst = libsbn.instance("burrito")
 
         # Read MCMC run to get tree structure.
@@ -110,8 +112,13 @@ class Burrito:
             px_phylo_log_like, px_theta_sample, px_branch_representation
         )
         # Get topology gradients.
-        sbn_grad = self.inst.topology_gradients(px_log_f)
-        self.opt.gradient_step({"scalar_params": scalar_grad, "sbn_params": sbn_grad})
+        sbn_grad = self.inst.topology_gradients(px_log_f, self.use_vimco)
+        self.opt.gradient_step(
+            {
+                "scalar_params": scalar_grad,
+                "sbn_params": sbn_grad
+            }
+        )
 
     def gradient_steps(self, step_count):
         with click.progressbar(range(step_count), label="Gradient descent") as bar:
