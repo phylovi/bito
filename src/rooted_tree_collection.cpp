d9613fd6f2a50eb0b8b3676cef627c1e230cdec1
Erick Matsen <ematsen@gmail.com>
2020-04-24 03:37:52 -0700

    Updating copyright to 2020.

--- src/rooted_tree_collection.cpp
+++ src/rooted_tree_collection.cpp
@@ -1,4 +1,4 @@
-// Copyright 2019 libsbn project contributors.
+// Copyright 2019-2020 libsbn project contributors.
 // libsbn is free software under the GPLv3; see LICENSE file for details.
 
 #include "rooted_tree_collection.hpp"
