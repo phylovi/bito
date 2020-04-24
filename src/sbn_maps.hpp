183c744221e88e016f8d814906196a15135e6ef0
Erick Matsen <ematsen@gmail.com>
2020-04-24 03:37:52 -0700

    Updating copyright to 2020.

--- src/sbn_maps.hpp
+++ src/sbn_maps.hpp
@@ -1,4 +1,4 @@
-// Copyright 2019 libsbn project contributors.
+// Copyright 2019-2020 libsbn project contributors.
 // libsbn is free software under the GPLv3; see LICENSE file for details.
 //
 // A collection of functions to handle the subsplit support and to turn trees into
