5823f5ec78c179e1173faddb139e57a66f8a3520
Erick Matsen <ematsen@gmail.com>
2020-04-26 03:10:49 -0700

    comments
    [skip travis]

--- src/taxon_name_munging.hpp
+++ src/taxon_name_munging.hpp
@@ -11,6 +11,9 @@ namespace TaxonNameMunging {
 std::string QuoteString(const std::string &in_str);
 std::string DequoteString(const std::string &in_str);
 TagStringMap DequoteTagStringMap(const TagStringMap &tag_string_map);
+
+// Parses dates as numbers appearing after an underscore. Returns a map specifying the
+// height of each taxon compared to the maximum date.
 TagDoubleMap TagDateMapOfTagTaxonMap(TagStringMap tag_taxon_map);
 }  // namespace TaxonNameMunging
 
@@ -34,6 +37,8 @@ TEST_CASE("TaxonNameMunging") {
   TagStringMap expected_test_map(
       {{2, unquoted_test}, {3, double_quoted_dequoted}, {5, single_quoted_dequoted}});
   CHECK_EQ(expected_test_map, DequoteTagStringMap(test_map));
+
+  // Test of TagDateMapOfTagTaxonMap appears in rooted_sbn_instance.hpp.
 }
 #endif  // DOCTEST_LIBRARY_INCLUDED
 
