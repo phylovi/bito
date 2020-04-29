326b09803341ec0397ddd1203e75fe2639a9a17e
Erick Matsen <ematsen@gmail.com>
2020-04-29 13:11:51 -0700

    final cleanup

--- src/lasagna.cpp
+++ src/lasagna.cpp
@@ -1,9 +1,6 @@
-#include <vector>
 #include "gp_operation.hpp"
 
 int main() {
-  // GPOperationVector z{GPOperations::Zero{5},
-  //                     GPOperations::WeightedSumAccumulate{5, 4, 5}};
   GPOperation z = GPOperations::Zero{5};
   std::cout << z << std::endl;
 }
