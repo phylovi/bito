dd4c155912858582f9f94dcc564d974c06882236
Erick Matsen <ematsen@gmail.com>
2020-05-01 15:57:46 -0700

    more cleanup; venus

--- src/lasagna.cpp
+++ src/lasagna.cpp
@@ -6,11 +6,11 @@
 
 using namespace GPOperations;
 
-// The "ancestor" node is the common ancestor of mars and saturn.
-enum HelloGPCSP { jupiter, mars, saturn, ancestor, root };
+// Let the "venus" node be the common ancestor of mars and saturn.
+enum HelloGPCSP { jupiter, mars, saturn, venus, root };
 
 // According to HelloGPCSP, this makes
-// (jupiter:0.113,(mars:0.15,saturn:0.1):0.22):0.;
+// (jupiter:0.113,(mars:0.15,saturn:0.1)venus:0.22):0.;
 GPInstance MakeHelloGPInstance() {
   GPInstance inst;
   inst.ReadFastaFile("data/hello.fasta");
@@ -47,7 +47,7 @@ TEST_CASE("GPInstance: straightforward classical likelihood calculation") {
       // Get common ancestor of mars and saturn.
       Multiply{PLV::ancestor_leaf, PLV::mars_root, PLV::saturn_root},
       // Evolve common ancestor rootward.
-      EvolveRootward{PLV::ancestor_root, PLV::ancestor_leaf, HelloGPCSP::ancestor},
+      EvolveRootward{PLV::ancestor_root, PLV::ancestor_leaf, HelloGPCSP::venus},
       // Evolve jupiter rootward.
       EvolveRootward{PLV::jupiter_root, PLV::jupiter_leaf, HelloGPCSP::jupiter},
       // Get root.
@@ -70,16 +70,16 @@ TEST_CASE("GPInstance: subsplit traversal as written") {
     p_saturn,      // fake subsplit leading to mars
     phat_stilde,   // root side of mars edge
     phat_s,        // root side of saturn edge
-    p_s,           // p at ancestor
+    p_s,           // p at venus
     phat_ttilde,   // root side of jupiter edge
-    phat_t,        // root side of ancestor edge
+    phat_t,        // root side of venus edge
     p_t,           // PLV at root from leaves
     rhat_t,        // stationary distribution coming from root
     r_ttilde,      // PLV pointing towards jupiter from root
-    r_t,           // PLV pointing towards ancestor from root
-    rhat_s,        // leaf side of ancestor edge
-    r_stilde,      // PLV pointing towards mars from ancestor
-    r_s,           // PLV pointing towards saturn from ancestor
+    r_t,           // PLV pointing towards venus from root
+    rhat_s,        // leaf side of venus edge
+    r_stilde,      // PLV pointing towards mars from venus
+    r_s,           // PLV pointing towards saturn from venus
     rhat_jupiter,  // Evolved message coming towards jupiter
     rhat_mars,     // Evolved message coming towards mars
     rhat_saturn,   // Evolved message coming towards saturn
@@ -92,11 +92,11 @@ TEST_CASE("GPInstance: subsplit traversal as written") {
       EvolveRootward{PLV::phat_stilde, PLV::p_mars, HelloGPCSP::mars},
       // Evolve saturn rootward.
       EvolveRootward{PLV::phat_s, PLV::p_saturn, HelloGPCSP::saturn},
-      // Get common ancestor of mars and saturn.
+      // Get rootward PLV for venus.
       Multiply{PLV::p_s, PLV::phat_stilde, PLV::phat_s},
-      // Evolve ancestor rootward.
-      EvolveRootward{PLV::phat_t, PLV::p_s, HelloGPCSP::ancestor},
-      // Get root PLV coming from leaves
+      // Evolve venus rootward.
+      EvolveRootward{PLV::phat_t, PLV::p_s, HelloGPCSP::venus},
+      // Get rootward PLV for root.
       Multiply{PLV::p_t, PLV::phat_ttilde, PLV::phat_t},
       // Set a stationary distribution coming from "beyond" the root.
       SetToStationaryDistribution{PLV::rhat_t},
@@ -108,12 +108,12 @@ TEST_CASE("GPInstance: subsplit traversal as written") {
       EvolveLeafward{PLV::rhat_jupiter, PLV::r_ttilde, HelloGPCSP::jupiter},
       // Calculate likelihood at jupiter.
       Likelihood{HelloGPCSP::jupiter, PLV::rhat_jupiter, PLV::p_jupiter},
-      // Get message going towards ancestor.
+      // Get message going towards venus.
       Multiply{PLV::r_t, PLV::phat_ttilde, PLV::rhat_t},
-      // Evolve the message towards ancestor.
-      EvolveLeafward{PLV::rhat_s, PLV::r_t, HelloGPCSP::ancestor},
-      // Calculate likelihood at common ancestor.
-      Likelihood{HelloGPCSP::ancestor, PLV::rhat_s, PLV::p_s},
+      // Evolve the message towards venus.
+      EvolveLeafward{PLV::rhat_s, PLV::r_t, HelloGPCSP::venus},
+      // Calculate likelihood at venus.
+      Likelihood{HelloGPCSP::venus, PLV::rhat_s, PLV::p_s},
       // Get message going towards mars.
       Multiply{PLV::r_stilde, PLV::rhat_s, PLV::phat_s},
       // Evolve the message towards mars.
@@ -126,16 +126,12 @@ TEST_CASE("GPInstance: subsplit traversal as written") {
       EvolveLeafward{PLV::rhat_saturn, PLV::r_s, HelloGPCSP::saturn},
       // Calculate likelihood at saturn.
       Likelihood{HelloGPCSP::saturn, PLV::rhat_saturn, PLV::p_saturn},
-
   };
 
   auto inst = MakeHelloGPInstance();
   auto engine = inst.GetEngine();
   engine->ProcessOperations(rootward_likelihood_calculation);
-  CHECK_LT(fabs(engine->GetLogLikelihoods()(HelloGPCSP::root) - -84.77961943), 1e-6);
-  CHECK_LT(fabs(engine->GetLogLikelihoods()(HelloGPCSP::mars) - -84.77961943), 1e-6);
-  CHECK_LT(fabs(engine->GetLogLikelihoods()(HelloGPCSP::jupiter) - -84.77961943), 1e-6);
-  CHECK_LT(fabs(engine->GetLogLikelihoods()(HelloGPCSP::saturn) - -84.77961943), 1e-6);
-  CHECK_LT(fabs(engine->GetLogLikelihoods()(HelloGPCSP::ancestor) - -84.77961943),
-           1e-6);
+  for (size_t idx = 0; idx <= root; idx++) {
+    CHECK_LT(fabs(engine->GetLogLikelihoods()(idx) - -84.77961943), 1e-6);
+  }
 }
