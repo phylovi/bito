498121042d9e6aa563ed9f8bfe36b0c188945709
Erick Matsen <ematsen@gmail.com>
2020-05-06 04:48:49 -0700

    dropping lseek and write
    [skip travis]

--- src/lasagna.cpp
+++ src/lasagna.cpp
@@ -3,16 +3,16 @@
 #include <string.h>
 #include <sys/mman.h>
 #include <unistd.h>
+#include "eigen_sugar.hpp"
 
 // https://www.youtube.com/watch?v=m7E9piHcfr4
 // https://jameshfisher.com/2017/01/28/mmap-file-write/
+// https://stackoverflow.com/a/43301909/467327
 
 int main(void) {
   int fd = open("_ignore/test_file", O_RDWR | O_CREAT, (mode_t)0600);
   const char *text = "hello";
   size_t textsize = strlen(text) + 1;
-  lseek(fd, textsize-1, SEEK_SET);
-  write(fd, "", 1);
   char *map = (char *)mmap(  //
       NULL,                  // This address is ignored because we are using MAP_SHARED.
       textsize,              // Size of map.
