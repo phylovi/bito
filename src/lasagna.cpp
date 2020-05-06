463af4aa9913d136da31d6f71b9722705af00148
Erick Matsen <ematsen@gmail.com>
2020-05-06 06:15:38 -0700

    writing a vector without crashing
    [skip travis]

--- src/lasagna.cpp
+++ src/lasagna.cpp
@@ -10,9 +10,36 @@
 // https://stackoverflow.com/a/43301909/467327
 
 int main(void) {
+  std::string fname("_ignore/test_file");
+  int fd = open(fname.c_str(), O_RDWR | O_CREAT, (mode_t)0600);
+  EigenVectorXd v(4);
+  v << 1, 2, 3, 4;
+  auto v_size = v.size() * sizeof(double);
+  char *map = (char *)mmap(  //
+      NULL,                  // This address is ignored because we are using MAP_SHARED.
+      v_size,                // Size of map.
+      PROT_READ | PROT_WRITE,  // We want to read and write
+      MAP_SHARED,              // We need MAP_SHARED to actually write to this.
+      fd,                      // File descriptor.
+      0                        // Offset.
+  );
+  memcpy(map, v.data(), v_size);
+  msync(map, v_size, MS_SYNC);
+  //  for (int i = 0; i < strlen(text); i++) {
+  //    printf("%c", map[i]);
+  //  }
+  munmap(map, v_size);
+  close(fd);
+  return 0;
+}
+
+int string_version(void) {
   int fd = open("_ignore/test_file", O_RDWR | O_CREAT, (mode_t)0600);
   const char *text = "hello";
   size_t textsize = strlen(text) + 1;
+  // This writes a string termination character.
+  lseek(fd, textsize - 1, SEEK_SET);
+  write(fd, "", 1);
   char *map = (char *)mmap(  //
       NULL,                  // This address is ignored because we are using MAP_SHARED.
       textsize,              // Size of map.
