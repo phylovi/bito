07b647b65408683d514b3562ab4dec4a41418f56
junseonghwan <s2jun.uw@gmail.com>
2020-05-26 18:16:30 -0700

    Working version of likelihood EM training.

--- src/gp_instance.hpp
+++ src/gp_instance.hpp
@@ -4,6 +4,9 @@
 #ifndef SRC_GP_INSTANCE_HPP_
 #define SRC_GP_INSTANCE_HPP_
 
+#include <memory.h>
+
+#include "dag_node.hpp"
 #include "gp_engine.hpp"
 #include "rooted_tree_collection.hpp"
 #include "sbn_maps.hpp"
@@ -19,7 +22,9 @@ class GPInstance {
   void ReadNexusFile(std::string fname);
 
   void MakeEngine();
+  void MakeGPEngine();
   GPEngine *GetEngine() const;
+  void TrainLikelihoodEM(double tol, size_t max_iter);
 
  private:
   std::string mmap_file_path_;
@@ -35,16 +40,51 @@ class GPInstance {
   // and PCSS bitsets are at the end.
   // The collection of rootsplits, with the same indexing as in the indexer_.
   BitsetVector rootsplits_;
-  // A map going from the index of a PCSS to its child.
+  // A map going from the index of a PCSP to its child.
   SizeBitsetMap index_to_child_;
   // A map going from a parent subsplit to the range of indices in
   // sbn_parameters_ with its children. See the definition of Range for the indexing
   // convention.
   BitsetSizePairMap parent_to_range_;
 
+  // The first entries are reserved for fake subsplits.
+  // We will adopt the convention of denoting a trivial subsplit by placing
+  // 0's in the first chunk.
+  // The next set of entries store the rootsplits.
+  // The remaining entries - 1 are for all other subsplits.
+  // The last entry is saved for pseudoroot -- for passing stationary.
+  // The pseudoroot is a Bitset with all 1's.
+  std::unordered_map<Bitset, size_t> subsplit_to_index_;
+  std::vector<std::shared_ptr<DAGNode>> dag_nodes_;
+
+  // Build indexing scheme for q_, branch_lengths_, log_likelihoods_.
+  // PCSP to index.
+  BitsetSizeMap gp_engine_indexer_;
+  // Stores range of indices for a subsplit and rotated subsplit.
+  BitsetSizePairMap subsplit2range_;
+
   void ClearTreeCollectionAssociatedState();
   void CheckSequencesAndTreesLoaded() const;
   void ProcessLoadedTrees();
+  
+  void CreateAndInsertNode(const Bitset &subsplit);
+  void ConnectNodes(size_t idx, bool rotated);
+  void AddChildrenSubsplits(const Bitset &subsplit, std::deque<Bitset> &q);
+  std::vector<Bitset> GetChildrenSubsplits(const Bitset &subsplit,
+                                           bool include_fake_subsplits = false);
+  void BuildNodes();
+  void BuildEdges();
+  void PrintDAG();
+  std::vector<size_t> LeafwardPassTraversal();
+  std::vector<size_t> RootwardPassTraversal();
+  void ConstructDAG();
+  void BuildGPEngineIndexer(BitsetSizeMap &indexer,
+                            BitsetSizePairMap &subsplit2range);
+  void RootwardPass(std::vector<size_t> visit_order, bool optimize);
+  void LeafwardPass(std::vector<size_t> visit_order, bool optimize);
+  void InitializeLikelihoodEM();
+  void SetZero();
+
 };
 
 #endif  // SRC_GP_INSTANCE_HPP_
