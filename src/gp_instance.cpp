9c00de3f88ca5d8935143b1928bf1835b1bcdb38
junseonghwan <s2jun.uw@gmail.com>
2020-08-05 20:43:10 -0700

    Remove Leaves encoding from the comment.

--- src/gp_instance.cpp
+++ src/gp_instance.cpp
@@ -182,13 +182,14 @@ RootedTreeCollection GPInstance::GenerateCompleteRootedTreeCollection() {
   Node::NodePtrVec topologies = dag_.GenerateAllGPNodeIndexedTopologies();
   const EigenVectorXd gpcsp_indexed_branch_lengths = engine_->GetBranchLengths();
 
-  // Map from Leaves-encoding to parent-subsplit encoding.
-  std::unordered_map<const Node *, Bitset> leaves_to_subsplit_indexer;
-  for (const auto &topology : topologies) {
-    topology->PreOrder([this, &leaves_to_subsplit_indexer](const Node *node) {
+  // Construct Node pointer to parent subsplit encoding.
+  // We will use this indexer to look up the GPCSP index.
+  std::unordered_map<const Node *, Bitset> node_to_subsplit_indexer;
+  for (size_t i = 0; i < trees.size(); i++) {
+    trees.at(i)->PreOrder([this, &node_to_subsplit_indexer](const Node *node) {
       GPDAGNode *dag_node = dag_.GetDagNode(node->Id());
-      if (leaves_to_subsplit_indexer.count(node) == 0) {
-        SafeInsert(leaves_to_subsplit_indexer, node, dag_node->GetBitset());
+      if (!node_to_subsplit_indexer.count(node)) {
+        SafeInsert(node_to_subsplit_indexer, node, dag_node->GetBitset());
       }
     });
   }
@@ -201,16 +202,15 @@ RootedTreeCollection GPInstance::GenerateCompleteRootedTreeCollection() {
     std::vector<double> branch_lengths(node_count);
 
     root_node->PreOrder(
-        [this, &branch_lengths, &gpcsp_indexed_branch_lengths,
-         &leaves_to_subsplit_indexer](const Node *node) {
+        [this, &branch_lengths, &bl, &node_to_subsplit_indexer](const Node *node) {
           const Node::NodePtrVec &children = node->Children();
           Assert(children.size() == 2 || children.empty(),
                  "Number of children must equal to 2 for the internal nodes and 0 for "
                  "the leaves.");
-          auto &parent_subsplit = leaves_to_subsplit_indexer.at(node);
-          for (const auto &child_node_shared : children) {
-            const Node *child_node = child_node_shared.get();
-            auto &child_subsplit = leaves_to_subsplit_indexer.at(child_node);
+          auto &parent_subsplit = node_to_subsplit_indexer.at(node);
+          for (size_t i = 0; i < children.size(); i++) {
+            const Node *child_node = children.at(i).get();
+            auto &child_subsplit = node_to_subsplit_indexer.at(child_node);
 
             // Note: child_subsplit is either a rotated or sorted subsplit of
             // parent_subsplit.
