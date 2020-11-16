8dab68763c3d879d170ad4eaec54cfd2ab1c3af5
Erick Matsen <ematsen@gmail.com>
2020-11-16 12:49:38 -0800

    Test passes, but surprised by missing support

--- src/rooted_sbn_instance.hpp
+++ src/rooted_sbn_instance.hpp
@@ -182,11 +182,17 @@ TEST_CASE("RootedSBNInstance: subsplit support and TrainSimpleAverage") {
   CHECK_EQ(correct_parameters, inst.PrettyIndexedSBNParameters());
 }
 
-TEST_CASE("RootedSBNInstance: TrainSimpleAverage on 20 taxa") {
+// Instance SA-trained on a sample of 20-taxon trees.
+RootedSBNInstance MakeRootedSimpleAverageInstance() {
   RootedSBNInstance inst("rooted instance");
   inst.ReadNewickFile("data/rooted_simple_average.nwk");
   inst.ProcessLoadedTrees();
   inst.TrainSimpleAverage();
+  return inst;
+}
+
+TEST_CASE("RootedSBNInstance: TrainSimpleAverage on 20 taxa") {
+  auto inst = MakeRootedSimpleAverageInstance();
   auto results = inst.PrettyIndexedSBNParameters();
   // Values confirmed with
   // https://github.com/mdkarcher/vbsupertree/commit/b7f87f711e8a1044b7c059b5a92e94c117d8cee1
@@ -325,6 +331,17 @@ TEST_CASE("RootedSBNInstance: reading SBN parameters from a CSV") {
   inst.SetSBNParameters({});
   CHECK_EQ(inst.sbn_parameters_[10], DOUBLE_MINIMUM);
   CHECK_THROWS(inst.SetSBNParameters({{"10000|01111|00001", -5.}}));
+}
+
+TEST_CASE("RootedSBNInstance: SBN parameter round trip") {
+  std::string csv_test_file_path = "_ignore/for_sbn_parameter_round_trip.csv";
+  auto inst = MakeRootedSimpleAverageInstance();
+  auto original_normalized_sbn_parameters = inst.NormalizedSBNParameters();
+  inst.SBNParametersToCSV(csv_test_file_path);
+  inst.ReadSBNParametersFromCSV(csv_test_file_path);
+  auto reloaded_normalized_sbn_parameters = inst.NormalizedSBNParameters();
+  CheckVectorXdEquality(original_normalized_sbn_parameters,
+                        reloaded_normalized_sbn_parameters, 1e-6);
 }
 
 #endif  // DOCTEST_LIBRARY_INCLUDED
