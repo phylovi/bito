33a45cb0eb588542d9f65a59a6022b4995dd75ff
Erick Matsen <ematsen@gmail.com>
2021-01-04 03:54:09 -0800

    prior test cutoff

--- src/gp_doctest.cpp
+++ src/gp_doctest.cpp
@@ -384,18 +384,18 @@ TEST_CASE("GPInstance: Priors") {
   // 0001|1110|0110, 4
   // 0001|1110|0010, 5
   auto support = inst.GetDAG().BuildUniformOnTopologicalSupportPrior();
-  CHECK_LT(fabs(support[0] - 2. / 3.), 1e-6);
-  CHECK_LT(fabs(support[1] - 1. / 3.), 1e-6);
-  CHECK_LT(fabs(support[4] - 1. / 2.), 1e-6);
-  CHECK_LT(fabs(support[5] - 1. / 2.), 1e-6);
+  CHECK_LT(fabs(support[0] - 2. / 3.), 1e-10);
+  CHECK_LT(fabs(support[1] - 1. / 3.), 1e-10);
+  CHECK_LT(fabs(support[4] - 1. / 2.), 1e-10);
+  CHECK_LT(fabs(support[5] - 1. / 2.), 1e-10);
   auto all = inst.GetDAG().BuildUniformOnAllTopologiesPrior();
   // There are 15 topologies on 4 taxa.
   // There are 3 topologies on 3 taxa, so there are 3 topologies with rootsplit
   // 0001|1110.
-  CHECK_LT(fabs(all[0] - 3. / 15.), 1e-6);
+  CHECK_LT(fabs(all[0] - 3. / 15.), 1e-10);
   // There is only 1 topology with rootsplit 0011|1100.
-  CHECK_LT(fabs(all[1] - 1. / 15.), 1e-6);
+  CHECK_LT(fabs(all[1] - 1. / 15.), 1e-10);
   // There are 3 topologies on 3 taxa.
-  CHECK_LT(fabs(all[4] - 1. / 3.), 1e-6);
-  CHECK_LT(fabs(all[5] - 1. / 3.), 1e-6);
+  CHECK_LT(fabs(all[4] - 1. / 3.), 1e-10);
+  CHECK_LT(fabs(all[5] - 1. / 3.), 1e-10);
 }
