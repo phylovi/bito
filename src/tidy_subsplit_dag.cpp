99189836776ca7cc7c94396b65c084a4d912adbe
Erick Matsen <ematsen@gmail.com>
2021-01-21 14:13:52 -0800

    Struggling a little with bool Eigen

--- src/tidy_subsplit_dag.cpp
+++ src/tidy_subsplit_dag.cpp
@@ -5,6 +5,8 @@
 
 TidySubsplitDAG::TidySubsplitDAG() : SubsplitDAG() {}
 
+TidySubsplitDAG::TidySubsplitDAG(EigenMatrixXb above) : above_(above){};
+
 TidySubsplitDAG::TidySubsplitDAG(const RootedTreeCollection &tree_collection)
     : SubsplitDAG(tree_collection) {}
 
@@ -13,6 +15,8 @@ EigenArrayXbRef TidySubsplitDAG::AboveNode(size_t node_idx) {
 }
 
 EigenArrayXbRef TidySubsplitDAG::BelowNode(size_t node_idx) {
-  return above_.block(node_idx, 1, 1, above_.cols()).array();
+  // This block is a way of getting the node_idx'th row.
+  return above_.block(node_idx, 0, 1, above_.cols()).array();
+  // I don't know why we can't do this:
   // return above_.row(node_idx).array();
 }
--- src/tidy_subsplit_dag.hpp
+++ src/tidy_subsplit_dag.hpp
@@ -11,6 +11,8 @@
 class TidySubsplitDAG : public SubsplitDAG {
  public:
   TidySubsplitDAG();
+  // This constructor is really just meant for testing.
+  explicit TidySubsplitDAG(EigenMatrixXb above);
   explicit TidySubsplitDAG(const RootedTreeCollection &tree_collection);
 
   EigenArrayXbRef AboveNode(size_t node_idx);
@@ -20,5 +22,28 @@ class TidySubsplitDAG : public SubsplitDAG {
   // above_.(i,j) is true if i is above j, otherwise it's false.
   EigenMatrixXb above_;
 };
+
+#ifdef DOCTEST_LIBRARY_INCLUDED
+TEST_CASE("TidySubsplitDAG: slicing") {
+  EigenMatrixXb above(3, 3);
+  above.setConstant(false);
+  above << true, false, true,  //
+      false, true, true,       //
+      false, false, false;
+  // std::cout << GenericToString(above) << std::endl;
+  auto dag = TidySubsplitDAG(above);
+  // AboveNode is working
+  std::cout << GenericToString(dag.AboveNode(0));
+  std::cout << GenericToString(dag.AboveNode(1));
+  std::cout << GenericToString(dag.AboveNode(2));
+  std::cout << std::endl;
+  // BelowNode is not
+  std::cout << GenericToString(dag.BelowNode(0));
+  std::cout << GenericToString(dag.BelowNode(1));
+  std::cout << GenericToString(dag.BelowNode(2));
+  CHECK_EQ(GenericToString(dag.AboveNode(1)), "[0, 1, 0]\n");
+  CHECK_EQ(GenericToString(dag.BelowNode(1)), "[0, 1, 1]\n");
+}
+#endif  // DOCTEST_LIBRARY_INCLUDED
 
 #endif  // SRC_TIDY_SUBSPLIT_DAG_HPP_
