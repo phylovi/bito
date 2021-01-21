3a45bc3766bb5114dbeb9d92729c9e49f898dbf0
Erick Matsen <ematsen@gmail.com>
2021-01-21 10:54:27 -0800

    tidy_subsplit_dag stub

---
+++ src/tidy_subsplit_dag.cpp
@@ -0,0 +1,9 @@
+// Copyright 2019-2020 libsbn project contributors.
+// libsbn is free software under the GPLv3; see LICENSE file for details.
+
+#include "tidy_subsplit_dag.hpp"
+
+TidySubsplitDAG::TidySubsplitDAG() : SubsplitDAG() {}
+
+TidySubsplitDAG::TidySubsplitDAG(const RootedTreeCollection &tree_collection)
+    : SubsplitDAG(tree_collection) {}
---
+++ src/tidy_subsplit_dag.hpp
@@ -0,0 +1,17 @@
+// Copyright 2019-2020 libsbn project contributors.
+// libsbn is free software under the GPLv3; see LICENSE file for details.
+//
+// A "tidy" subsplit DAG has a notion of clean and dirty vectors.
+
+#ifndef SRC_TIDY_SUBSPLIT_DAG_HPP_
+#define SRC_TIDY_SUBSPLIT_DAG_HPP_
+
+#include "subsplit_dag.hpp"
+
+class TidySubsplitDAG : public SubsplitDAG {
+ public:
+  TidySubsplitDAG();
+  explicit TidySubsplitDAG(const RootedTreeCollection &tree_collection);
+};
+
+#endif  // SRC_TIDY_SUBSPLIT_DAG_HPP_
