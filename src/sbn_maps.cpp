95698ea133f9a0acddd36582db026e9b129b7dc4
Erick Matsen <ematsen@gmail.com>
2021-01-30 05:46:44 -0800

    Update copyright to 2021 (#315)

--- src/sbn_maps.cpp
+++ src/sbn_maps.cpp
@@ -1,4 +1,4 @@
-// Copyright 2019-2020 libsbn project contributors.
+// Copyright 2019-2021 libsbn project contributors.
 // libsbn is free software under the GPLv3; see LICENSE file for details.
 
 #include "sbn_maps.hpp"
