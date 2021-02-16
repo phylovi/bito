739030e2a93c46c5ba4d19a474f00bc73c19d3eb
Mathieu Fourment <m.fourment@gmail.com>
2021-02-17 08:15:30 +1100

    Renaming variables

--- src/rooted_sbn_instance.hpp
+++ src/rooted_sbn_instance.hpp
@@ -185,9 +185,9 @@ TEST_CASE("RootedSBNInstance: subsplit support and TrainSimpleAverage") {
                                          {"00010|00101|00001", 1},
                                          {"00001|01110|00100", 1},
                                          {"00010|11101|00100", 1}});
-  sort(correct_parameters.begin(), correct_parameters.end());
+  std::sort(correct_parameters.begin(), correct_parameters.end());
   auto parameters = inst.PrettyIndexedSBNParameters();
-  sort(parameters.begin(), parameters.end());
+  std::sort(parameters.begin(), parameters.end());
   CHECK_EQ(correct_parameters, parameters);
 }
 
@@ -364,12 +364,12 @@ TEST_CASE("RootedSBNInstance: reading SBN parameters from a CSV") {
   auto inst = MakeFiveTaxonRootedInstance();
   inst.ReadSBNParametersFromCSV("data/test_modifying_sbn_parameters.csv");
   auto pretty_indexer = inst.PrettyIndexer();
-  auto it = find(pretty_indexer.begin(), pretty_indexer.end(), "10000|01111|00001");
-  CHECK(it != pretty_indexer.end());
-  auto index = std::distance(pretty_indexer.begin(), it);
-  CHECK_LT(fabs(inst.sbn_parameters_[index] - log(0.15)), 1e-8);
+  auto gpcsp_it = std::find(pretty_indexer.begin(), pretty_indexer.end(), "10000|01111|00001");
+  CHECK(gpcsp_it != pretty_indexer.end());
+  auto gpcsp_idx = std::distance(pretty_indexer.begin(), gpcsp_it);
+  CHECK_LT(fabs(inst.sbn_parameters_[gpcsp_idx] - log(0.15)), 1e-8);
   inst.SetSBNParameters({}, false);
-  CHECK_EQ(inst.sbn_parameters_[index], DOUBLE_MINIMUM);
+  CHECK_EQ(inst.sbn_parameters_[gpcsp_idx], DOUBLE_MINIMUM);
   CHECK_THROWS(inst.SetSBNParameters({{"10000|01111|00001", -5.}}, false));
 }
 
