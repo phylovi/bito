f0aa12faca5f2966489a4d6d9346aabb10ed2154
Erick Matsen <ematsen@gmail.com>
2022-01-14 07:46:52 -0800

    printing some things for test

--- src/gp_doctest.cpp
+++ src/gp_doctest.cpp
@@ -288,6 +288,10 @@ TEST_CASE("GPInstance: hotstart branch lengths") {
       0.1892030000, 0.1894900000, 0.1895430000, 0.1896900000, 0.1905710000;
   double true_mean = hotstart_expected_branch_lengths.array().mean();
   inst.HotStartBranchLengths();
+  auto indexer = inst.PrettyIndexer();
+  inst.PrintDAG();
+  std::cout << inst.GetEngine()->GetBranchLengths();
+  std::cout << true_mean;
   CHECK_EQ(true_mean, inst.GetEngine()->GetBranchLengths()(2));
 }
 
