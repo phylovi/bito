dd937e7e5bd53b873337598b2864d450f91967af
Erick Matsen <ematsen@gmail.com>
2022-01-14 16:10:05 -0800

    proof of concept for building trees as indices

--- src/noodle.cpp
+++ src/noodle.cpp
@@ -1,4 +1,6 @@
 #include "unrooted_sbn_instance.hpp"
+#include "rooted_sbn_instance.hpp"
+#include "gp_instance.hpp"
 
 // This is just a place to muck around, and check out performance.
 
@@ -9,19 +11,23 @@ auto now = std::chrono::high_resolution_clock::now;
 // gprof2dot -f callgrind callgrind.out.16763 | dot -Tpng -o ~/output.png
 
 int main() {
-  uint32_t leaf_count = 10000;
+  GPInstance gp_inst("_ignore/mmapped_plv.data");
+  gp_inst.ReadNewickFile("data/five_taxon_rooted.nwk");
+  gp_inst.ReadFastaFile("data/five_taxon.fasta");
+  gp_inst.MakeEngine();
+  auto tree_collection = gp_inst.GenerateCompleteRootedTreeCollection();
+  auto indexer = gp_inst.GetDAG().BuildGPCSPIndexer();
 
-  Node::NodePtr topology = Node::Ladder(leaf_count);
-
-  std::vector<size_t> ids;
-  ids.reserve(1 + 2 * leaf_count);
-
-  auto t_start = now();
-  for (int i = 0; i < 100; i++) {
-    ids.clear();
-    topology->Preorder([&ids](const Node* node) { ids.push_back(node->Id()); });
+  std::vector<RootedIndexerRepresentation> indexer_representations;
+  for (const auto &tree : tree_collection.Trees()) {
+    indexer_representations.push_back(
+        RootedSBNMaps::IndexerRepresentationOf(indexer, tree.Topology(), SIZE_MAX));
   }
-
-  std::chrono::duration<double> duration = now() - t_start;
-  std::cout << "time: " << duration.count() << " seconds\n";
+  std::cout << indexer_representations << std::endl;
+  UnrootedSBNInstance ur_inst("charlie");
+  ur_inst.ReadNewickFile("data/five_taxon_rooted.nwk");
+  ur_inst.ReadFastaFile("data/five_taxon.fasta");
+  PhyloModelSpecification simple_specification{"JC69", "constant", "strict"};
+  ur_inst.PrepareForPhyloLikelihood(simple_specification, 2);
+  std::cout << ur_inst.UnrootedLogLikelihoods(tree_collection);
 }
