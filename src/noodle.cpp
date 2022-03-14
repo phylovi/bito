a3c343ebe5c2f0c275c185db1eb114e97cb4bb44
Chris Jennings-Shaffer <chrisjenningsshaffer@gmail.com>
2022-03-14 15:33:26 -0700

    noodle now does pcsp index stuff

--- src/noodle.cpp
+++ src/noodle.cpp
@@ -12,22 +12,41 @@ auto now = std::chrono::high_resolution_clock::now;
 // valgrind --tool=callgrind ./_build/noodle
 // gprof2dot -f callgrind callgrind.out.16763 | dot -Tpng -o ~/output.png
 
-// StringDoubleMap StringDoubleMapOfStringDoubleVector(StringDoubleVector vect) {
-//   StringDoubleMap m;
-//   for (const auto& [str, x] : vect) {
-//     SafeInsert(m, str, x);
-//   }
-//   return m;
-// }
+void LoadIndexerRepresentations(
+    std::vector<RootedIndexerRepresentation> &indexer_representations,
+    PreRootedTreeCollection &trees, BitsetSizeMap &indexer) {
+  for (const auto &tree : trees.Trees()) {
+    indexer_representations.push_back(
+        RootedSBNMaps::IndexerRepresentationOf(indexer, tree.Topology(), SIZE_MAX));
+  }
+}
 
-
+void WriteTreesToFile(
+    const std::string &out_path,
+    const std::vector<RootedIndexerRepresentation> &representations,
+    const std::vector<double> &log_likelihoods = {}) {
+  std::ofstream out_stream(out_path);
+  out_stream << std::setprecision(12);
+  const auto write_likelihood = !log_likelihoods.empty();
+  for (size_t which_tree = 0; which_tree < representations.size(); which_tree++) {
+    for (const auto &idx : representations.at(which_tree)) {
+      out_stream << idx << ",";
+    }
+    if (write_likelihood) {
+      out_stream << log_likelihoods.at(which_tree);
+    }
+    out_stream << "\n";
+  }
+  out_stream.close();
+}
 
 int main(int argc, char *argv[]) {
-  //std::cout << argc << std::endl;
+  // std::cout << argc << std::endl;
   if (argc != 8) {
-    std::cout << "We need exactly 7 arguments: fasta, rooted_nwk, credible_rooted_nwk,"
-                "pp_rooted_nwk, repr_out_path, credible_repr_out_path, and pp_repr_out_path"
-              << std::endl;
+    std::cout
+        << "We need exactly 7 arguments: fasta, rooted_nwk, credible_rooted_nwk,"
+           "pp_rooted_nwk, repr_out_path, credible_repr_out_path, and pp_repr_out_path"
+        << std::endl;
     abort();
   }
   std::string fasta_path = argv[1];
@@ -39,79 +58,36 @@ int main(int argc, char *argv[]) {
   std::string pp_out_path = argv[7];
   auto thread_count = std::thread::hardware_concurrency();
 
-  GPInstance gp_inst("mmapped_plv.data");
-  gp_inst.ReadNewickFile(rooted_nwk_path);
-  gp_inst.ReadFastaFile(fasta_path);
-  gp_inst.MakeEngine();
-  gp_inst.TakeFirstBranchLength();
-  auto tree_collection = gp_inst.GenerateCompleteRootedTreeCollection();
+  GPInstance all_trees_gp_inst("mmapped_plv.data");
+  RootedSBNInstance cred_r_inst("cred_trees");
+  RootedSBNInstance pp_r_inst("pp_trees");
+  all_trees_gp_inst.ReadNewickFile(rooted_nwk_path);
+  all_trees_gp_inst.ReadFastaFile(fasta_path);
+  all_trees_gp_inst.MakeEngine();
+  all_trees_gp_inst.TakeFirstBranchLength();
+  cred_r_inst.ReadNewickFile(credible_rooted_nwk_path);
+  pp_r_inst.ReadNewickFile(pp_rooted_nwk_path);
 
-  auto indexer = gp_inst.GetDAG().BuildGPCSPIndexer();
-  std::vector<RootedIndexerRepresentation> indexer_representations;
-  for (const auto &tree : tree_collection.Trees()) {
-    indexer_representations.push_back(
-        RootedSBNMaps::IndexerRepresentationOf(indexer, tree.Topology(), SIZE_MAX));
-  }
- 
+  auto all_trees = all_trees_gp_inst.GenerateCompleteRootedTreeCollection();
+  auto cred_trees = cred_r_inst.tree_collection_;
+  auto pp_trees = pp_r_inst.tree_collection_;
+  auto indexer = all_trees_gp_inst.GetDAG().BuildGPCSPIndexer();
+  std::vector<RootedIndexerRepresentation> all_representations;
+  std::vector<RootedIndexerRepresentation> cred_representations;
+  std::vector<RootedIndexerRepresentation> pp_representations;
+  LoadIndexerRepresentations(all_representations, all_trees, indexer);
+  LoadIndexerRepresentations(cred_representations, cred_trees, indexer);
+  LoadIndexerRepresentations(pp_representations, pp_trees, indexer);
+
   UnrootedSBNInstance ur_inst("charlie");
   ur_inst.ReadNewickFile(rooted_nwk_path);
   ur_inst.ReadFastaFile(fasta_path);
   PhyloModelSpecification simple_specification{"JC69", "constant", "strict"};
-  ur_inst.PrepareForPhyloLikelihood(simple_specification, thread_count, {}, true, 
-                                    tree_collection.TreeCount());
-  const auto log_likelihoods = ur_inst.UnrootedLogLikelihoods(tree_collection);
+  ur_inst.PrepareForPhyloLikelihood(simple_specification, thread_count, {}, true,
+                                    all_trees.TreeCount());
+  const auto log_likelihoods = ur_inst.UnrootedLogLikelihoods(all_trees);
 
-  std::ofstream out_stream(out_path);
-  out_stream << std::setprecision(12);
-  for (size_t which_tree = 0; which_tree < tree_collection.TreeCount(); which_tree++) {
-    for (const auto &idx : indexer_representations.at(which_tree)) {
-      out_stream << idx << ",";
-    }
-    out_stream << log_likelihoods.at(which_tree) << "\n";
-  }
-  out_stream.close();
-
-  //We also need the representations of the credible_set topologies.
-  RootedSBNInstance r_inst("just_a_name");
-  r_inst.ReadNewickFile(credible_rooted_nwk_path);
-  tree_collection = r_inst.tree_collection_;
-
-  std::vector<RootedIndexerRepresentation> cred_indexer_representations;
-  for (const auto &tree : tree_collection.Trees()) {
-    cred_indexer_representations.push_back(
-        RootedSBNMaps::IndexerRepresentationOf(indexer, tree.Topology(), SIZE_MAX));
-  }
-  out_stream.open(credible_out_path);
-  for (size_t which_tree = 0; which_tree < tree_collection.TreeCount(); which_tree++) {
-    for (const auto &idx : cred_indexer_representations.at(which_tree)) {
-      out_stream << idx << ",";
-    }
-    out_stream << std::endl;
-  }
-  out_stream.close();
-
-
-
-  //We also need the representations of the topologies with pp values.
-  RootedSBNInstance r_inst1("again");
-  r_inst1.ReadNewickFile(pp_rooted_nwk_path);
-  tree_collection = r_inst1.tree_collection_;
-
-  std::vector<RootedIndexerRepresentation> cred_indexer_representations1;
-  for (const auto &tree : tree_collection.Trees()) {
-    cred_indexer_representations1.push_back(
-        RootedSBNMaps::IndexerRepresentationOf(indexer, tree.Topology(), SIZE_MAX));
-  }
-  out_stream.open(pp_out_path);
-  for (size_t which_tree = 0; which_tree < tree_collection.TreeCount(); which_tree++) {
-    for (const auto &idx : cred_indexer_representations1.at(which_tree)) {
-      out_stream << idx << ",";
-    }
-    out_stream << std::endl;
-  }
-  out_stream.close();
-
-
-
-
+  WriteTreesToFile(out_path, all_representations, log_likelihoods);
+  WriteTreesToFile(credible_out_path, cred_representations);
+  WriteTreesToFile(pp_out_path, pp_representations);
 }
