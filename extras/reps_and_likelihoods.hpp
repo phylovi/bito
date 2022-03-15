a389046a2b6a74b6f901dc2d6aa449cb3aa02576
Chris Jennings-Shaffer <chrisjenningsshaffer@gmail.com>
2022-03-15 11:47:51 -0700

    Moved new functionality of noodle.cpp to reps_and_likelihoods.cpp (and .hpp)

---
+++ extras/reps_and_likelihoods.hpp
@@ -0,0 +1,27 @@
+// Copyright 2019-2022 bito project contributors.
+// bito is free software under the GPLv3; see LICENSE file for details.
+//
+// The purpose of this executable is to determine likelihoods and PCSP indexer 
+// representations of various trees as part of a proof of concept for nni sdag support. 
+// Specifically, this takes in a fasta file and three newick files of rooted trees with
+// branch lengths. A subsplit DAG is built on the first set of trees and this is used
+// for the indexer representations of all three sets of trees. When a subsplit appears 
+// in the second or third set of trees, but not the subsplit DAG, the subsplit has 
+// SIZE_MAX as the index.
+
+#pragma once
+
+#include "../src/unrooted_sbn_instance.hpp"
+#include "../src/rooted_sbn_instance.hpp"
+#include "../src/gp_instance.hpp"
+#include <thread>
+
+
+void LoadIndexerRepresentations(
+    std::vector<RootedIndexerRepresentation> &indexer_representations,
+    PreRootedTreeCollection &trees, BitsetSizeMap &indexer);
+
+void WriteTreesToFile(
+    const std::string &out_path,
+    const std::vector<RootedIndexerRepresentation> &representations,
+    const std::vector<double> &log_likelihoods = {});
