594f447e0b0e96b4834ecff112426c896751179b
Hassan Nasif <20205159+hrnasif@users.noreply.github.com>
2022-06-10 15:41:08 -0700

    update link in doctest

--- src/gp_doctest.cpp
+++ src/gp_doctest.cpp
@@ -41,7 +41,7 @@ GPInstance GPInstanceOfFiles(
 // Our tree is (see check below)
 // (jupiter:0.113,(mars:0.15,saturn:0.1)venus:0.22):0.;
 // You can see a helpful diagram at
-// https://github.com/phylovi/libsbn/issues/213#issuecomment-624195267
+// https://github.com/phylovi/bito/issues/349#issuecomment-898672399
 GPInstance MakeHelloGPInstance(const std::string& fasta_path) {
   auto inst = GPInstanceOfFiles(fasta_path, "data/hello_rooted.nwk");
   EigenVectorXd branch_lengths(5);
