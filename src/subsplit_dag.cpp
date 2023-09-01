e18a8193eadf77ffd8e870ffafaba91d5d67b377
David Rich <david.rich@umontana.edu>
2023-09-01 09:40:09 -0700

    PR changes.

--- src/subsplit_dag.cpp
+++ src/subsplit_dag.cpp
@@ -1716,8 +1716,6 @@ NodeIdVectorPair SubsplitDAG::FindChildNodeIdsViaScan(const Bitset &subsplit,
 }
 
 NodeIdVectorPair SubsplitDAG::FindParentNodeIds(const Bitset &subsplit) const {
-  return FindParentNodeIdsViaScan(subsplit);
-
   auto [left, right] = FindParentNodeIdsViaMap(subsplit);
   // If DAG contains grafted nodes, linearly scan for grafted nodes parents.
   if (storage_.HaveHost()) {
@@ -1727,8 +1725,6 @@ NodeIdVectorPair SubsplitDAG::FindParentNodeIds(const Bitset &subsplit) const {
 }
 
 NodeIdVectorPair SubsplitDAG::FindChildNodeIds(const Bitset &subsplit) const {
-  return FindChildNodeIdsViaScan(subsplit);
-
   auto [left, right] = FindChildNodeIdsViaMap(subsplit);
   // If DAG contains grafted nodes, linearly scan for grafted nodes children.
   if (storage_.HaveHost()) {
